9bbea279178881708639e33bfc53c9953d89bdfb
Anastasia Yim <yim.anast@gmail.com>
2020-08-20 19:16:30 -0400

    08/20/2020

---
+++ BugKM:CM.js
@@ -0,0 +1,10 @@
+function cockroachSpeed(s) {
+  let result = Math.floor(s * 27.7778);
+  return result
+}
+
+//math.floor "returns the largest integer
+//less than or equal to a given number."
+
+//And we wanted the answer to be an integer - and
+//the input to be a decimal ("Real Number")
