e4f1afb9ee828e0bab1815c57d9debfb46c680b5
Anastasia Yim <yim.anast@gmail.com>
2020-08-26 12:29:28 -0400

    08/26/2020

---
+++ 08:26:2020.js
@@ -0,0 +1,11 @@
+//Grasshopper - Messi Goals
+
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = (43+10+5);
+
+function sum(){
+  return (totalGoals)
+}
