5a0090978699e6c5445a5e24919f2938d0b90f8d
Anastasia Yim <yim.anast@gmail.com>
2020-08-27 12:21:58 -0400

    08/27/2020

---
+++ 08:27:2020.js
@@ -0,0 +1,9 @@
+function arr(n){
+  let newArr = []; //create an array
+  for(var i = 0; i < n; i++){
+    newArr.push(i); //add i (the integer) to the array!! and then updates the length
+  }
+  return newArr;
+}
+
+//arr is secretly defined bc I can't use "Array" for a variable name ?
