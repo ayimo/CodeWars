b0468ccd8fcd0b559fbbb14eb439a167787abbc4
Anastasia Yim <yim.anast@gmail.com>
2020-09-11 14:57:25 -0400

    09/11/2020

---
+++ 09:11:2020.js
@@ -0,0 +1,9 @@
+function squareArea(A){
+  const r = 2 * A / Math.PI;
+
+  return Math.round(r * r * 100) / 100;
+}
+
+//Complete the function that calculates the area
+//of the red square, when the length of the circular
+//arc A is given as the input. Return the result rounded to two decimals.
