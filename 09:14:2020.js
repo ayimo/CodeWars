efa940e0597ae096322a811314c8f8e2963181d0
Anastasia Yim <yim.anast@gmail.com>
2020-09-15 16:56:42 -0400

    Added question

--- 09:14:2020.js
+++ 09:14:2020.js
@@ -1,6 +1,6 @@
 // Write a function that receives the speed (in cm/s)
-// of a petal as input, and
-// returns the time it takes for that petal to reach the ground from the same branch.
+// of a petal as input, and returns the time
+//it takes for that petal to reach the ground from the same branch.
 
 function sakuraFall(v) {
   const speed = v;
