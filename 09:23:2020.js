a2a8b3c9b51963104701754fe389a44f39e59d00
Anastasia Yim <yim.anast@gmail.com>
2020-09-23 17:38:40 -0400

    09/23/2020

---
+++ 09:23:2020.js
@@ -0,0 +1,11 @@
+function tripleTrouble(one, two, three){
+  let result = ""
+  for(i = 0 ; i <= one.length ; i++){
+   result += one.charAt(i) + two.charAt(i) + three.charAt(i)
+  }
+  return result
+ }
+
+
+//  for(i = 0 ; i <= one.length ; i++)
+//  for(i = 0 ; i <= one.substring(0) ; i++)
